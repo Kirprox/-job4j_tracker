8408998a0f2302604c8ac23747468326956f8963
kir <kirpro95@yandex.ru>
2024-01-11 16:53:07 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("is active " + active);
+        System.out.println("status " + status);
+        System.out.println("message " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        System.out.println();
+        Error error2 = new Error(true, 2, "Critical error");
+        error2.printInfo();
+    }
+}
