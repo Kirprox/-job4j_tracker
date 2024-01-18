f1cc9aabd14b3578fca55bf31e483949895b9412
kir <kirpro95@yandex.ru>
2024-01-18 11:06:43 +0300

    Theory

---
+++ src/main/java/ru/job4j/oop/BirdMain.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class BirdMain {
+    public static void main(String[] args) {
+        Bird parrot = new Bird() {
+            public void flyInside() {
+                System.out.println("Попугай летает только внутри анонимного класса");
+            }
+
+            @Override
+            public void fly() {
+                System.out.println("летит попугой");
+                flyInside();
+            }
+
+        };
+        parrot.fly();
+    }
+}
