115768071855575d0c09809b7f50de4ff5ebe2e3
kir <kirpro95@yandex.ru>
2024-01-20 10:53:38 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 464);
+        Book headFirst = new Book("Head First java", 720);
+        Book theCompleteRef = new Book("Java. The Complete Reference", 1300);
+        Book algorithm = new Book("Algorithms in Java", 520);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = headFirst;
+        books[2] = theCompleteRef;
+        books[3] = algorithm;
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.println(book.getName() + ", " + book.getNumOfPages() + " pages");
+        }
+        System.out.println("Shown only Clean code name books");
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + ", " + book.getNumOfPages() + " pages");
+            }
+        }
+    }
+}
+
