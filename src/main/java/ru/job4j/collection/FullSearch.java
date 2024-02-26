cf79b91beda9b22ac8850fe5d5aa02cd96e70da9
kir <kirpro95@yandex.ru>
2024-02-26 21:36:53 +0300

    1. Уникальные задачи. [#212653 #483231]

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -1,17 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class FullSearch {
-    public List<String> extractNumber(List<Task> tasks) {
-        Set<Task> set = new HashSet<>(tasks);
-        List<String> rsl = new ArrayList<>();
-        for (Task task : set) {
-            rsl.add(task.getNumber());
+    public Set<String> extractNumber(List<Task> tasks) {
+        HashSet<String> set = new HashSet<>();
+        for (Task task : tasks) {
+            set.add(task.getNumber());
         }
-        return rsl;
+        return set;
     }
 }
