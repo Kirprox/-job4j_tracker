d8e68d25fd721c5974d8964246de11a75c0931ff
kir <kirpro95@yandex.ru>
2024-03-08 13:15:34 +0300

    0. Map, HashMap [#212652 #485005]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("kirpro95@yandex.ru", "Razhin Kirill Vladimirovich");
+        map.put("kirpro95@yandex.ru", "Petrov Kirill Sergeevich");
+        map.put("kirprox9595@gmail.com", "Ivanov Kirill Andreevich");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
