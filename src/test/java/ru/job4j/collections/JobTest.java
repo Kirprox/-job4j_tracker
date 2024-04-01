8286905b2bd7af0eebf76e73987245418ccb0181
kir <kirpro95@yandex.ru>
2024-04-01 09:51:38 +0300

    2. Комбинированный компаратор [#10036 #489074]

--- src/test/java/ru/job4j/collections/JobTest.java
+++ src/test/java/ru/job4j/collections/JobTest.java
@@ -11,8 +11,8 @@ class JobTest {
     public void whenCompatorByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("first task", 1),
+                new Job("first task", 0)
         );
         assertThat(rsl).isLessThan(0);
     }
@@ -21,8 +21,8 @@ class JobTest {
     public void whenCompatorAscByPriorityAndByName() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1),
+                new Job("Impl task", 0)
         );
         assertThat(rsl).isGreaterThan(0);
     }
